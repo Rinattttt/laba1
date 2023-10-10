c52b9c567ac71042647335eead53eb3b39c837a4
Rinattttt <146668608+Rinattttt@users.noreply.github.com>
2023-10-10 21:46:20 +0300

    Add files via upload

---
+++ poisk vremeni padenia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите H");// подсказка
+            double H = Convert.ToDouble(Console.ReadLine());// ввод значения высоты для переменной H
+            double g = 9.8;// велечина ускорения свободного падения
+            double x = 2 * H / g;// присвоение части вычислений по формуле промежуточной переменной
+            double T = Math.Sqrt(x);// извлечение числа из под корня(также по формуле) = присвоение переменной т значение нужного нам результата
+            Console.WriteLine($" Время падения камня на поверхность земли с высоты h = { T }");// вывод нужного нам результата закрепленного за переменной Т
+            Console.ReadKey();// команда для показа интерфейса консоли после исполнения программы/кода
+        }
+    }
+}
