4c72feae3fbff5178e6064e2560896b48c5c7f03
Rinattttt <146668608+Rinattttt@users.noreply.github.com>
2023-10-17 22:41:01 +0300

    Add files via upload

---
+++ число н.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum = sum + i * 10 + 1;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
