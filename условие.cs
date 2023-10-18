7724ecc76de2f36f0b1cf952576677144d82b0a5
Rinattttt <146668608+Rinattttt@users.noreply.github.com>
2023-10-18 10:34:06 +0300

    Add files via upload

---
+++ условие.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число градусов первого угла данного треугольника");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число градусов второго угла данного треугольника");
+            int B = Convert.ToInt32(Console.ReadLine());
+            if ((A + B) < 180)
+            {
+                Console.WriteLine("Такой треугольник существует");
+                if (A == 90 || B == 90)
+                {
+
+                    Console.WriteLine("Треугольник прямоугольный");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такой треугольник не существует");
+            }
+        }
+    }
+}
