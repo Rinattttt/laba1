a96d6f6b9d441f0cbbceec90b64f2095080aa655
Rinattttt <146668608+Rinattttt@users.noreply.github.com>
2023-10-24 12:54:37 +0300

    Add files via upload

---
+++ 4 пр р.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_пр_работа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int i = 1;
+            void a()
+            {
+                i++;
+            }
+            while (i <= n)
+            {
+                sum += i * 10 + 1;
+                a();  
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+                  
+        
+    
