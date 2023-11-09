6c8861b5c01ad17aaef87399d594745615103fbb
Rinattttt <146668608+Rinattttt@users.noreply.github.com>
2023-11-09 19:20:49 +0300

    Add files via upload

---
+++ четные элеменеты массива.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace четные_элементы_массива
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввиедите количество строк");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int[,] massiv = new int[a, b];
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    Console.WriteLine("Введите каждое значение для каждого элемента массива");
+                    massiv[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Все четные значения элементов массива(разделенные пробелом):");
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    if (massiv[i, j] % 2 == 0)
+                    {
+                        Console.Write(massiv[i, j] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
