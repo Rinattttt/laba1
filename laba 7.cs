01697a7eba407d0a19533bfe00c6cf8b606fad7a
Rinattttt <146668608+Rinattttt@users.noreply.github.com>
2023-11-14 18:49:54 +0300

    Add files via upload

---
+++ laba 7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+
+            if (int.TryParse(str, out int celoe_chislo))
+            {
+                Console.WriteLine(1);
+            }
+            else if (double.TryParse(str, out double vehchestv_chislo))
+            {
+                Console.WriteLine(2);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
